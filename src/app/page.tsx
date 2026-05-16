eecd8648e43a288c10ff61bcdbdb1e2365de667c
promotion-pal <promotion.work.pal@gmail.com>
2026-05-16 19:22:44 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,14 +18,14 @@ export default function Index() {
 
         <div className="container relative z-20">
           <div className="max-w-3xl">
-            <h1 className="text-2xl md:text-7xl font-bold mb-6">
+            <h1 className="text-3xl md:text-7xl font-bold mb-6">
               Профессиональный груминг
-              <span className="block text-3xl md:text-4xl mt-2">
+              <span className="block text-xl md:text-4xl mt-2">
                 для ваших питомцев
               </span>
             </h1>
 
-            <p className="text-lg md:text-xl text-gray-200 mb-8 leading-relaxed">
+            <p className="text-lg md:text-xl mb-8 leading-relaxed">
               Забота, любовь и профессиональный уход за вашими питомцами. Более
               5 лет опыта, тысячи довольных клиентов и их хвостатых друзей.
             </p>
